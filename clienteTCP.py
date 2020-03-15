430e801f95e31d85074da72d416beb2cd911e1db
Vilma Tirado Gomez <vm.tirado@uniandes.edu.co>
2020-03-15 15:19:41 -0500

    first commit

---
+++ clienteTCP.py
@@ -0,0 +1,68 @@
+# Import socket module
+import socket
+import logging
+import hashlib
+import time
+from _thread import *
+from threading import Thread
+def Main():
+    m=hashlib.sha1()
+    #Initializes the client log
+    logging.basicConfig(filename="clientLog.log", level=logging.INFO,
+    format = '%(asctime)s %(levelname)-8s %(message)s',
+    datefmt = '%Y-%m-%d %H:%M:%S'
+    )
+    numt=int(input("Cuantos threads de cliente quiere correr?"))
+    for i in range(numt):
+        print("entre al for")
+        try:
+            thread = Thread(target = threaded, args = (i,m,))
+            thread.start()
+            thread.join()
+        except:
+            print("Error al arrancar el thread")
+
+
+def threaded(i,m):
+    print("Entre al threaded")
+    start_time=time.time()
+    # local host IP '127.0.0.1'
+    host = '127.0.0.1'
+    # Define the port on which you want to connect
+    port = 6666
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # connect to server on local computer
+    s.connect((host, port))
+    print("Thread #%d Ready to receive info ",i)
+    logging.info("Thread #%d ready to receive info",i)
+    f=open("ctext.txt",'wb')
+    while True:
+        # message received from server
+        data = s.recv(1024)
+        #f.write(data)
+        print(data)
+        if not data:
+            print("Termino el envio")
+            break
+        elif (data.__contains__(b"HASH")):
+            logging.info("Found hash")
+            index=data.find(b"HASH")
+            m.update(data[:index])
+            print("Los datos usados son ", data[:index])
+            realM=data[index+4:]
+            print("Hash recibido ",realM.decode())
+            print("Hash creado ",m.hexdigest())
+            if m.hexdigest() == realM.decode():
+                print("Hash correcto")
+                logging.info("Envio exitoso")
+            else:
+                print("Hash incorrecto")
+                logging.info("Envio corrupto ")
+
+        m.update(data)
+
+    logging.info("Tiempo del envio %s",(time.time()-start_time))
+    logging.info("---------------------------------------------")
+    s.close()
+
+Main()
---
+++ servidorTCP.py
@@ -0,0 +1,89 @@
+# import socket programming library 
+import socket
+
+# import thread module 
+from _thread import *
+import threading
+import logging
+import hashlib
+#print_lock = threading.Lock()
+
+# thread function
+# c is the socket object with which connection was made, used to send and receive messages
+def threaded(c):
+    m = hashlib.sha1()
+    print("El archivo que se va a abrir es: ",file)
+    with open(file,'rb') as f:
+        while True:
+            # data received from client
+            data= f.read()
+
+            if not data:
+                print("Termine de mandar")
+
+                #envio el hash
+                h=str(m.hexdigest())
+                print("el digest que mando es ",m.hexdigest())
+                c.send(("HASH"+h).encode())
+                break
+            # send back reversed string to client
+            print("Mando: ",data)
+            m.update(data)
+            c.send(data)
+
+     # connection closed
+    c.close()
+
+
+def Main():
+    #Initializes the server log
+    logging.basicConfig(filename="serverLog.log", level=logging.INFO,
+    format = '%(asctime)s %(levelname)-8s %(message)s',
+    datefmt = '%Y-%m-%d %H:%M:%S'
+    )
+    host = "localhost"
+    # reverse a port on your computer
+    # in our case it is 12345 but it
+    # can be anything
+    port = 6666
+    logging.info('Connected to %s on port %s', host,port)
+    # Socket del servidor
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+    print("socket binded to port %s", port)
+
+    # put the socket into listening mode 
+    s.listen(5)
+    print("socket is listening")
+    texto=int (input("\n Que texto desea enviar?"
+                     "\n 1. 100 mib"
+                     "\n 2. 250 mib"))
+    if texto==1:
+        f="text1.txt"
+    else:
+        f="text2.txt"
+    global file
+    file=f
+    print("Archivo seleccionado: ",file)
+    num_conn= int( input('\n Cuantas conexiones desea recibir?' ))
+
+    print("Esperando conexiones:")
+
+    #Counter with number of conection receibed
+    cont=0
+    while cont<=num_conn :
+        # establish connection with client
+        # c is a socket object to send and receive messages
+        c, addr = s.accept()
+        # lock acquired by client 
+        #print_lock.acquire()
+        print('Connected to :', addr[0], ':', addr[1])
+
+        # Start a new thread and return its identifier 
+        start_new_thread(threaded, (c,))
+        cont+=1
+    s.close()
+
+
+
+Main()
